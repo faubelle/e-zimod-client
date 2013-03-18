ea937507a5ffa59b56a15d4fc207957621f5bc37
Romain Edelmann <romain.edelmann@gmail.com>
2013-03-18 21:04:04 +0100

    Added Flot, for comparison with gRaphael.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,149 @@
-
 $(function() {
 
-	var r = Raphael("chart");
+    var r = Raphael("chart");
 
-	function showChart(elements) {
+    function showChart(elements) {
 
-		r.clear();
+        // Flot
 
-		var xs = $.map(elements, function(x, i) { return [$.map(x, function(y, j) { return [y[1]]; })]; });
-		var ys = $.map(elements, function(x, i) { return [$.map(x, function(y, j) { return [y[0]]; })]; });
+        var es = $.map(elements, function(e, i) {
+            return { data: e, label: " " }
+        });
 
-		var chart = r.linechart(50, 50, 700, 500, ys, xs, {smooth: false, symbol: 'circle', width: 1, axis: '0 0 1 1', shade: true});
-		chart.hoverColumn(function () {
-	        this.tags = r.set();
+        var plot = $.plot("#flot", es, {
+            hoverable: true,
+            shadowSize: 0,
+            crosshair: {
+                mode: "x"
+            },
+            grid: {
+                hoverable: true,
+                autoHighlight: false
+            },
+            xaxis: {
+                min: 0,
+                max: 3600,
+                tickSize: 600,
+                label: "Time (s)"
+            },
+            yaxis: {
+                label: "Power (W)"
+            },
+             series: {
+                lines: { show: true, fill: true }
+            }
+        });
 
-	        for (var i = 0, ii = this.y.length; i < ii; i++) {
-	            this.tags.push(r.flag(this.x, this.y[i], this.values[i] + " W", 0, 10).insertBefore(this));
-	        }
-	    }, function () {
-	        this.tags && this.tags.remove();
-	    });
+        var legends = $("#flot .legendLabel");
 
-	    return chart;
-	}
+        legends.each(function () {
+            // fix the widths so they don't jump around
+            $(this).css('width', $(this).width());
+        });
 
-	function getRandomProfiles(n) {
-		$.ajax({
-			url: "http://localhost:8000/randomProfile?callback=?",
-			data: {n: n},
-			dataType: "jsonp",
-			success: function(ps) {
-				showChart(ps);
-			},
-		});
-	}
+        var updateLegendTimeout = null;
+        var latestPosition = null;
 
-	$("#fetchBtn").click(function() {
-		getRandomProfiles(Math.min($("#n").val(), 30));
-	})
+        $("#flot").mouseleave(function() {
+            $("#flot .legend").hide();
+        });
+
+        $("#flot").mouseenter(function() {
+            $("#flot .legend").show();
+        });
+
+
+        function updateLegend() {
+
+            updateLegendTimeout = null;
+
+            var pos = latestPosition;
+
+            var axes = plot.getAxes();
+            if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+                pos.y < axes.yaxis.min || pos.y > axes.yaxis.max) {
+                return;
+            }
+
+            var i, j, dataset = plot.getData();
+            for (i = 0; i < dataset.length; ++i) {
+
+                var series = dataset[i];
+
+                // Find the nearest points, x-wise
+
+                for (j = 0; j < series.data.length; ++j) {
+                    if (series.data[j][0] > pos.x) {
+                        break;
+                    }
+                }
+
+                // Now Interpolate
+
+                var y,
+                    p1 = series.data[j - 1],
+                    p2 = series.data[j];
+
+                if (p1 == null) {
+                    y = p2[1];
+                } else if (p2 == null) {
+                    y = p1[1];
+                } else {
+                    y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+                }
+
+                legends.eq(i).text(series.label = (/.*/, "" + y.toFixed(2) + " W"));
+            }
+        }
+
+        $("#flot").bind("plothover",  function (event, pos, item) {
+            latestPosition = pos;
+            if (!updateLegendTimeout) {
+                updateLegendTimeout = setTimeout(updateLegend, 50);
+            }
+        });
+
+        // GRaphael
+
+        r.clear();
+
+        var xs = $.map(elements, function(x, i) { return [$.map(x, function(y, j) { return [y[1]]; })]; });
+        var ys = $.map(elements, function(x, i) { return [$.map(x, function(y, j) { return [y[0]]; })]; });
+
+        var chart = r.linechart(50, 50, 500, 300, ys, xs, {
+            smooth: false, 
+            symbol: 'circle', 
+            width: 1, 
+            axis: '0 0 1 1', 
+            shade: true,
+            axisxstep: 60,
+            axisystep: 5
+        });
+        chart.hoverColumn(function () {
+            this.tags = r.set();
+
+            for (var i = 0, ii = this.y.length; i < ii; i++) {
+                this.tags.push(r.flag(this.x, this.y[i], this.values[i] + " W", 0, 10).insertBefore(this));
+            }
+        }, function () {
+            this.tags && this.tags.remove();
+        });
+
+        return chart;
+    }
+
+    function getRandomProfiles(n) {
+        $.ajax({
+            url: "http://localhost:8000/randomProfile?callback=?",
+            data: {n: n},
+            dataType: "jsonp",
+            success: function(ps) {
+                showChart(ps);
+            },
+        });
+    }
+
+    $("#fetchBtn").click(function() {
+        getRandomProfiles(Math.min($("#n").val(), 30));
+    })
 });
